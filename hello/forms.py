ae43c6a54570de5bd4582fc91a519892b9fcab59
t17n <tt17noviembre@gmail.com>
2023-11-02 18:52:59 +0900

    section23_completed

---
+++ hello/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class HelloForm(forms.Form):
+    check = forms.BooleanField(label='Checkbox', required=False)
+    
+    
---
+++ hello/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from . forms import HelloForm
+
+class HelloView(TemplateView):
+    def __init__(self):
+        self.params = {
+            'title': 'Hello',
+            'form': HelloForm(),
+            'result':None
+        }
+
+    def get(self, request):
+        return render(request, 'hello/index.html', self.params)
+    
+    def post(self, request):
+        if ('check' in request.POST):
+            self.params['result'] = 'Checked!'
+        else:
+            self.params['result'] = 'not checked...'
+            self.params['form'] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
+    
