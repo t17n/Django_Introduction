a58270e91aebfe98da69b075808b3e1dc1267f8f
t17n <tt17noviembre@gmail.com>
2023-11-07 15:37:31 +0900

    section3-3 completed

--- hello/forms.py
+++ hello/forms.py
@@ -1,46 +1,4 @@
 from django import forms
 
 class HelloForm(forms.Form):
-    data=[
-        ('one', 'item 1'),
-        ('two', 'item 2'),
-        ('three', 'item 3'),
-        ('four', 'item 4'),
-        ('five', 'item 5'),
-    ]
-    choice = forms.MultipleChoiceField(label='radio', choices=data, widget=forms.SelectMultiple(attrs={'size': 6, 'class':'form-select'}))
-
-    '''
-    # 選択リスト
-    data=[
-        ('one', 'item 1'),
-        ('two', 'item 2'),
-        ('three', 'item 3'),
-        ('four', 'item 4'),
-        ('five', 'item 5'),
-    ]
-    choice = forms.ChoiceField(label='radio', choices=data, widget=forms.Select(attrs={'size': 5, 'class':'form-select'}))
-
-    # ラジオボタン
-    data=[
-        ('one', 'radio 1'),
-        ('two', 'radio 2'),
-        ('three', 'radio 3')
-    ]
-    choice = forms.ChoiceField(label='radio', choices=data, widget=forms.RadioSelect())
-
-    # プルダウン
-    data=[
-        ('one', 'item 1'), 
-        ('two', 'item 2'), 
-        ('three', 'item 3')
-    ]
-    choice = forms.ChoiceField(label='Choice', choices=data)
-    
-    # チェックボックス
-    check = forms.NullBooleanField(label='Check')
-    '''
-
-# session
-class SessionForm(forms.Form):
-    session = forms.CharField(label='session', required=False, widget=forms.TextInput(attrs={'class':'form-control'}))
+    id = forms.IntegerField(label='ID')
--- hello/views.py
+++ hello/views.py
@@ -1,31 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from .forms import SessionForm
+from .models import Friend
+from django.db.models import QuerySet
 
-class HelloView(TemplateView):
-    def __init__(self):
-        self.params = {
-            'title': 'Hello', 
-            'form': SessionForm(), 
-            'result': None
-        }
-    
-    def get(self, request):
-        self.params['result'] = request.session.get('last_msg', 'No message.')
-        return render(request, 'hello/index.html', self.params)
-    
-    def post(self, request):
-        ses = request.POST['session']
-        self.params['result'] = 'send: "' + ses + '".'
-        request.session['last_msg'] = ses
-        self.params['form'] = SessionForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
-    
-    def sample_middleware(get_response):
-        def middleware(request):
-            counter = request.session.get('counter', 0)
-            request.session['counter'] = counter + 1
-            response = get_response(request)
-            print("count: " + str(counter))
-            return response
-        return middleware
+def __new_str__(self):
+    result = ''
+    for item in self:
+        result += '<tr>'
+        for k in item:
+            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
+        result += '</tr>'
+    return result
+
+QuerySet.__str__ = __new_str__
+
+def index(request):
+    data = Friend.objects.all().values('id', 'name', 'age')
+    params = {
+        'title': 'Hello',
+        'data':data,
+    }
+    return render(request, 'hello/index.html', params)
