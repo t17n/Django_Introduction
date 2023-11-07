fa03a947865f1bc3f632cdab3c5dec10de7573e5
t17n <tt17noviembre@gmail.com>
2023-11-08 00:16:05 +0900

    section3-4 completed

--- hello/views.py
+++ hello/views.py
@@ -1,22 +1,83 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Friend
-from django.db.models import QuerySet
+from .forms import FriendForm
 
-def __new_str__(self):
-    result = ''
-    for item in self:
-        result += '<tr>'
-        for k in item:
-            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
-        result += '</tr>'
-    return result
+# ★
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
-QuerySet.__str__ = __new_str__
+class FriendList(ListView):
+    model = Friend
+
+class FriendDetail(DetailView):
+    model = Friend
+
+
+
 
 def index(request):
-    data = Friend.objects.all().values('id', 'name', 'age')
+    data = Friend.objects.all()
     params = {
         'title': 'Hello',
-        'data':data,
+        'data': data,
     }
     return render(request, 'hello/index.html', params)
+
+# create model(modelsForm)
+def create(request):
+    if (request.method == 'POST'):
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'form': FriendForm(),
+    }
+    return render(request, 'hello/create.html', params)
+
+def edit(request, num):
+    obj = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'id':num,
+        'form': FriendForm(instance=obj),
+    }
+    return render(request, 'hello/edit.html', params)
+
+def delete(request, num):
+    friend = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend.delete()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'id': num,
+        'obj': friend,
+    }
+    return render(request, 'hello/delete.html', params)
+
+
+'''
+# create model(1)
+def create(request):
+    params = {
+        'title':'Hello',
+        'form': HelloForm(),
+    }
+    if (request.method == 'POST'):
+        name = request.POST['name']
+        mail = request.POST['mail']
+        gender = 'gender' in request.POST
+        age = int(request.POST['age'])
+        birth = request.POST['birthday']
+        friend = Friend(name=name,mail=mail,gender=gender,age=age,birthday=birth)
+        friend.save()
+        return redirect(to='/hello')
+    return render(request, 'hello/create.html', params)
+'''
